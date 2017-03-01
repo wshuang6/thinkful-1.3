074d9d1e94584320943cd6981cab75ab47053bbd
wshuang6 <seraph617@gmail.com>
2017-03-01 17:26:40 -0500

    finished drill 7

--- ObjectDrills/objectDrills7.js
+++ ObjectDrills/objectDrills7.js
@@ -16,7 +16,7 @@ function printLOTRweapons(characters){
   characters.forEach(
       function(person){
         if(person.weapon){
-          console.log(`${person.name} is a ${person.role} who uses a ${person.weapon}`);
+          console.log(`${person.name} is a ${person.role} who uses a ${person.weapon}.`);
         }else {
           console.log(`${person.name} is a ${person.role} who doesn't have a weapon.`);
         }
@@ -24,23 +24,33 @@ function printLOTRweapons(characters){
   );
 }
 
-function isCharacter(objectPerson, name) {
-  if (objectPerson.name === name){
-    console.log('what is this ' + objectPerson);
-  return objectPerson;
-  }
-}
-
-function addWeapon(database, name, weapon){
-  console.log(database.find(isCharacter, name));
-
-}
-
 charactersLOTR.push(
   {name:'Arwen Undomiel', role:'Half-Elf of Rivendell'}
-);
+); //adds Arwen
+
+charactersLOTR[0].weapon = "wizard staff";
+charactersLOTR[1].weapon = "the Ring";
+charactersLOTR[2].weapon = "String and Barrow Blade";
+charactersLOTR[3].weapon = "Anduril";
+charactersLOTR[4].weapon = "Bow and Arrow";
+charactersLOTR[5].weapon = "Hadhafang"; //adds weapons
+
+//complicated attempts
+// function isCharacter(objectPerson, name) {
+//   if (objectPerson.name === name){
+//     console.log('what is this ' + objectPerson);
+//   return objectPerson;
+//   }
+// }
+
+// function addWeapon(database, name, weapon){
+//   console.log(database.find(isCharacter, name));
+
+// }
+
+
 
 printLOTR(charactersLOTR);
 printLOTRweapons(charactersLOTR);
 //console.log(charactersLOTR);
-addWeapon(charactersLOTR, 'Gandolf the White', 'wizard staff');
+//addWeapon(charactersLOTR, 'Gandolf the White', 'wizard staff');
