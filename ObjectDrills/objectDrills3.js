ab9c5205edbcded21faaa79c0720b5e1aac28fe0
Alvin Law <alvinlaw@gmail.com>
2017-03-01 11:51:38 -0800

    Finish Object Drills 3

---
+++ ObjectDrills/objectDrills3.js
@@ -0,0 +1,6 @@
+//Arrays in object
+var myObject = {
+  meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
+};
+
+console.log(`Fourth Meal of the day:  ${myObject.meals[3]}`);
